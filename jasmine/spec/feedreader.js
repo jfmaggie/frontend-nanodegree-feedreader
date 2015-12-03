c15ac40fb0116979cbea90713f1617b748bf2870
Maggie <jfmaggie@gmail.com>
2015-12-02 23:43:07 -0800

    fix 11th

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -71,7 +71,7 @@ $(function() {
         */
         it('visibility is changed', function() {
             $('.menu-icon-link').trigger('click');
-            expect($('body').hasClass('.menu-hidden')).toBe(false);
+            expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
@@ -79,18 +79,21 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
 
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
